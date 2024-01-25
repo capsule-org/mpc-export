7bd2c0d8e95cfe7932b6f6962a47ce91b10fea19
nsquare3 <113948443+nsquare3@users.noreply.github.com>
2024-01-25 10:02:47 -0800

    vendor deps

--- go.mod
+++ go.mod
@@ -2,14 +2,11 @@ module github.com/capsule-org/mpc-export-internal
 
 go 1.19
 
-require (
-	github.com/capsule-org/go-sdk v0.20.0
-	github.com/capsule-org/multi-party-sig v0.0.2-0.20230928201053-edf633b509c1
-	github.com/google/uuid v1.5.0
-)
+require github.com/capsule-org/go-sdk v0.21.0
 
 require (
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/capsule-org/multi-party-sig v0.0.2-0.20240124180317-3ef16283509b // indirect
 	github.com/cronokirby/saferith v0.33.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
 	github.com/ethereum/go-ethereum v1.10.25 // indirect
--- main.go
+++ main.go
@@ -2,123 +2,13 @@ package main
 
 import (
 	"encoding/hex"
-	"errors"
 	"fmt"
 	"os"
 	"regexp"
 	"strings"
-	"sync"
 
 	mpcsigner "github.com/capsule-org/go-sdk/signer"
-	"github.com/capsule-org/multi-party-sig/pkg/ecdsa"
-	"github.com/capsule-org/multi-party-sig/pkg/party"
-	"github.com/capsule-org/multi-party-sig/pkg/pool"
-	"github.com/capsule-org/multi-party-sig/pkg/protocol"
-	"github.com/capsule-org/multi-party-sig/protocols/doerner"
-	"github.com/capsule-org/multi-party-sig/protocols/doerner/keygen"
-	"github.com/google/uuid"
 )
-
-type Network struct {
-	parties          party.IDSlice
-	listenChannels   map[party.ID]chan *protocol.Message
-	closedListenChan chan *protocol.Message
-	mtx              sync.Mutex
-}
-
-func newNetwork(parties party.IDSlice) *Network {
-	closed := make(chan *protocol.Message)
-	close(closed)
-	c := &Network{
-		parties:          parties,
-		listenChannels:   make(map[party.ID]chan *protocol.Message, 2*len(parties)),
-		closedListenChan: closed,
-	}
-	return c
-}
-
-func (n *Network) init() {
-	N := len(n.parties)
-	for _, id := range n.parties {
-		n.listenChannels[id] = make(chan *protocol.Message, N*N)
-	}
-}
-
-func (n *Network) next(id party.ID) <-chan *protocol.Message {
-	n.mtx.Lock()
-	defer n.mtx.Unlock()
-	if len(n.listenChannels) == 0 {
-		n.init()
-	}
-	c, ok := n.listenChannels[id]
-	if !ok {
-		return n.closedListenChan
-	}
-	return c
-}
-
-func (n *Network) send(msg *protocol.Message) {
-	n.mtx.Lock()
-	defer n.mtx.Unlock()
-	for id, c := range n.listenChannels {
-		if msg.IsFor(id) && c != nil {
-			n.listenChannels[id] <- msg
-		}
-	}
-}
-
-func handlerLoop(id party.ID, h protocol.Handler, network *Network) {
-	for {
-		select {
-		case msg, ok := <-h.Listen():
-			if !ok {
-				return
-			}
-			go network.send(msg)
-
-		case msg := <-network.next(id):
-			h.Accept(msg)
-		}
-	}
-}
-
-func runHandler(wg *sync.WaitGroup, id party.ID, handler protocol.Handler, network *Network) {
-	defer wg.Done()
-	handlerLoop(id, handler, network)
-}
-
-func runSign(hash []byte, configSender *keygen.ConfigSender, configReceiver *keygen.ConfigReceiver) (*ecdsa.Signature, error) {
-	partyIDs := party.IDSlice{"USER", "CAPSULE"}
-	pl := pool.NewPool(0)
-	defer pl.TearDown()
-
-	session := uuid.New()
-	handlerUser, err := protocol.NewTwoPartyHandler(doerner.SignReceiver(configReceiver, partyIDs[0], partyIDs[1], hash, pl), session[:], true)
-	if err != nil {
-		return nil, err
-	}
-	handlerCapsule, err := protocol.NewTwoPartyHandler(doerner.SignSender(configSender, partyIDs[1], partyIDs[0], hash, pl), session[:], true)
-	if err != nil {
-		return nil, err
-	}
-
-	var wg sync.WaitGroup
-	network := newNetwork(partyIDs)
-	wg.Add(2)
-	go runHandler(&wg, partyIDs[0], handlerUser, network)
-	go runHandler(&wg, partyIDs[1], handlerCapsule, network)
-	wg.Wait()
-
-	result, err := handlerUser.Result()
-	if err != nil {
-		return nil, err
-	}
-	sig, ok := result.(*ecdsa.Signature)
-	if !ok {
-		return nil, errors.New("error casting result to Signature")
-	}
-	return sig, nil
-}
 
 func main() {
 	fmt.Println("\n\n---------------- Generating private key with backup share ----------------\n")
